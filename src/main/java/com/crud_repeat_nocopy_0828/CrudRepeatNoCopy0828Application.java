64e399060f0d799a0d7e0b19aad9bb70c4279c92
ehy0419 <2hy0419@naver.com>
2025-08-28 13:28:15 +0900

    Feat. 세팅

---
+++ src/main/java/com/crud_repeat_nocopy_0828/CrudRepeatNoCopy0828Application.java
@@ -0,0 +1,13 @@
+package com.crud_repeat_nocopy_0828;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRepeatNoCopy0828Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRepeatNoCopy0828Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/crud_repeat_nocopy_0828/CrudRepeatNoCopy0828ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_repeat_nocopy_0828;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRepeatNoCopy0828ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
